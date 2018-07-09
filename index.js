cca8269dd5dd00a952a4ca60d901120fc57900d4
Maxence Cornet <maxence.cornet20@gmail.com>
2018-07-09 21:54:51 +0200

    Expose methods to detect Brave Browser publishers using both API, DNS lookup and txt file fetching

--- index.js
+++ index.js
@@ -1,54 +1,55 @@
-var top_websites = require('./top-1m.json');
 var Promise = require("bluebird");
 var _ = require('underscore');
 const dns = require('dns');
+var rp = require('request-promise');
 
-i = 2;
-j = 1002;
-var range = _.range(i, j);
-
-let scrap_top_1m_websites = (range) => {
-  return Promise.all(range.map((key) => {
-    const current_website = top_websites["" + key + ""]['google.com'];
-    return get_dns_record(current_website);
-  })).then((res) => {
-    console.log('Finished range', range[0]);
-    return res;
-  });
-}
-
-var get_dns_record = (website) => {
-  //console.log('DNS for: ', website)
+let is_website_verified_by_dns_record = (website_url) => {
   return new Promise((resolve, reject) => {
     return dns.resolve(website, 'TXT', function (err, records) {
-      if (!err) {
-        records.forEach(element => {
-          if (element[0].indexOf('brave-ledger-verification=') !== -1) {
-            console.log('DETECTED: ', website);
-          }
-        });
+      if (err) {
+        return reject(err);
       }
-      resolve();
+      records.forEach(element => {
+        if (element[0].indexOf('brave-ledger-verification=') !== -1) {
+          return resolve(true);
+        } else {
+          return resolve(false);
+        }
+      });
     });
   })
 }
 
-var main = () => {
-  console.log('TESTING RANGE:', range[0]);
-  return scrap_top_1m_websites(range).then(() => {
-    if (j > 1000000) {
-      console.log('JOB DONE');
-      return;
-    }
-    i = i + 1000;
-    j = j + 1000;
-    range = _.range(i, j);
-    main();
-  })
+let is_website_verified_by_txt_verification_file = (website_url) => {
+  return rp(website_url + '/.well-known/brave-payments-verification.txt')
+    .then(function (res) {
+      res = JSON.parse(res);
+      if (res.statusCode === '200') {
+        return true;
+      } else {
+        return false;
+      }
+    })
+    .catch(function (err) {
+      return Promise.reject(err);
+    });
 }
 
-main();
+let is_website_verified_by_brave_api_call = (website_url) => {
+  return rp('https://mercury-proxy.privateinternetaccess.com/v3/publisher/identity?publisher=' + website_url)
+    .then(function (res) {
+      res = JSON.parse(res);
+      if (res.properties.verified) {
+        return true;
+      }
+    })
+    .catch(function (err) {
+      return Promise.reject(err);
+    });
+}
 
-
-
-
+module.exports = {
+  is_website_verified_by_dns_record,
+  is_website_verified_by_txt_verification_file,
+  is_website_verified_by_brave_api_call,
+};
